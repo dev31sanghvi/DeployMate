a198ec7599712904e57a7390f6a84fcd10da4fbb
Dev Sanghvi <devsanghvi06@gmail.com>
2024-07-22 10:16:18 +0530

    made env imports

--- src/aws.ts
+++ src/aws.ts
@@ -1,9 +1,12 @@
 import { S3 } from "aws-sdk";
 import fs from "fs";
+import dotenv from "dotenv";
 
 // aws credentials
-
+// s3 bucket
+dotenv.config();
 const s3 = new S3({
+  // region: "us-east-1",
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   endpoint: "endpoint url ",
@@ -14,8 +17,9 @@ const s3 = new S3({
 //file path: /user/desktop/majorproject/deploymate/dist/output/123456/index.js
 
 export const uploadFile = async (filename: string, localfilepath: string) => {
+  //syncronpusly readfile
   const filecontent = fs.readFileSync(localfilepath);
-  // might take some time to uplod
+  // might take some time to upload
   const response = await s3
     .upload({
       // params
