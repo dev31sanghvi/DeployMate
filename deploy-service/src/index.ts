3993ad60a84c392910eb71b60c5f9a717fa5a96e
Dev Sanghvi <devsanghvi06@gmail.com>
2024-09-25 21:20:14 +0530

    implemented the redis queue logic for pushing the element

---
+++ deploy-service/src/index.ts
@@ -0,0 +1,23 @@
+import { createClient, commandOptions } from "redis";
+// import { copyFinalDist, downloadS3Folder } from "./aws";
+// import { buildProject } from "./utils";
+
+// this loop will pull the value from the build-queue(redis)
+const subscriber = createClient();
+subscriber.connect(); // locally redis is running on 6379 port
+
+async function main() {
+  while (1) {
+    // this is an infinitely running while loop
+
+    const response = await subscriber.brPop(
+      commandOptions({ isolated: true }),
+      "build-queue",
+      0 // 0 basically is a timestamp . It will wait for the value to be pushed in the queue
+    );
+    console.log(response);
+  }
+}
+main();
+
+//calling main function
