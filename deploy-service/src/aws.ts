c8b7c40bd8f4923b89e636b8023aa5378c90a636
Dev Sanghvi <105737978+dev31sanghvi@users.noreply.github.com>
2024-12-12 22:18:51 +0530

    Update aws.ts

--- deploy-service/src/aws.ts
+++ deploy-service/src/aws.ts
@@ -3,9 +3,9 @@ import fs from 'fs';
 import path from 'path';
 
 const s3 = new S3({
-  accessKeyId: '7ea9c3f8c7f0f26f0d21c5ce99d1ad6a',
+  accessKeyId: 'your accessKeyId',
   secretAccessKey:
-    'b4df203781dd711223ce931a2d7ca269cdbf81bb530de4548474584951b798be',
+    'your secretAccessKey',
   endpoint: 'https://e21220f4758c08sgbira9c388712d42ef2.r2.cloudflarestorage.com',
 });
 
